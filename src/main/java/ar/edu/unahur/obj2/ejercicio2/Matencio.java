ba97449e523fbcfcc5eda27f2fe656c62b6a785d
Julia Gambuzzi <jgambuzzi@jgambuzzi-ltm.internal.salesforce.com>
2019-12-13 13:42:48 -0300

    StringBuffer

--- src/main/java/ar/edu/unahur/obj2/ejercicio2/Matencio.java
+++ src/main/java/ar/edu/unahur/obj2/ejercicio2/Matencio.java
@@ -3,55 +3,56 @@ package ar.edu.unahur.obj2.ejercicio2;
 public class Matencio{
 
 
-    public void prepararMate(TipoMate tipoMate){
+    public String prepararMate(TipoMate tipoMate){
+        StringBuffer instrucciones = new StringBuffer();
+        calentarAgua(instrucciones);
+        llenarTermo(instrucciones);
+        agregarYerbaAlMate(instrucciones);
+        agregarSaboresSiHay(tipoMate, instrucciones);
+        mojarElMate(instrucciones);
+        ponerLaBombilla(instrucciones);
+        cebar(instrucciones);
 
-        calentarAgua();
-        llenarTermo();
-        agregarYerbaAlMate();
-        agregarSaboresSiHay(tipoMate);
-        mojarElMate();
-        ponerLaBombilla();
-        cebar();
-
+        return instrucciones.toString();
     }
 
-    private void calentarAgua(){
-        System.out.println("Calentar el agua a 80 grados");
+    private void calentarAgua(StringBuffer instrucciones ){
+        instrucciones.append("Calentar el agua a 80 grados");
     }
 
-    private void llenarTermo(){
-        System.out.println("Llene el termo con el agua caliente");
+    private void llenarTermo(StringBuffer instrucciones ){
+        instrucciones.append(("Llene el termo con el agua caliente");
     }
 
-    private void agregarYerbaAlMate(){
-        System.out.println("Incline el mate a 45 grados y llene 3/4 partes de yerba");
+    private void agregarYerbaAlMate(StringBuffer instrucciones ){
+        instrucciones.append(("Incline el mate a 45 grados y llene 3/4 partes de yerba");
     }
 
-    private void agregarSaboresSiHay(TipoMate tipoMate){
+    private void agregarSaboresSiHay(TipoMate tipoMate, StringBuffer instrucciones ){
 
         if(TipoMate.CITRIC.equals(tipoMate)){
-            System.out.println("Agregue las cáscaras de naranja a la yerba, mezcle");
+            instrucciones.append(("Agregue las cáscaras de naranja a la yerba, mezcle");
         }else if(TipoMate.MENTA.equals(tipoMate)){
-            System.out.println("Agregue la menta a la yerba, mezcle");
+            instrucciones.append(("Agregue la menta a la yerba, mezcle");
         }else if(TipoMate.PEPERINA.equals(tipoMate)){
-            System.out.println("Agregue la peperina a la yerba, mezcle");
+            instrucciones.append(("Agregue la peperina a la yerba, mezcle");
         }else if(TipoMate.DULCE.equals(tipoMate)){
-            System.out.println("Agregue azucar a la yerba, repita esta operación antes de cada cebada");
+            instrucciones.append(("Agregue azucar a la yerba, repita esta operación antes de cada cebada");
         }else {
             throw new RuntimeException("Tipo de mate inválido");
         }
     }
 
-    private void mojarElMate(){
-        System.out.println("Moje el mate lentamente del lado que el nivel de yerba es más bajo 3 veces dejando que la yerba se humedezca");
+    private void mojarElMate(StringBuffer instrucciones ){
+        instrucciones.append(("Moje el mate lentamente del lado que el nivel de yerba es más bajo 3 veces dejando que la yerba se humedezca");
     }
 
-    private void ponerLaBombilla(){
-        System.out.println("Inserte la bombilla en la yerba húmeda");
+    private void ponerLaBombilla(StringBuffer instrucciones ){
+        instrucciones.append(("Inserte la bombilla en la yerba húmeda");
     }
 
-    private void cebar(){
-        System.out.println("Ya puede comenzar la ronda");
+    private void cebar(StringBuffer instrucciones ){
+        instrucciones.append(("Ya puede comenzar la ronda");
     }
 
 
