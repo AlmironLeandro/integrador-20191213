42d50f7dc78aed55565350266822d2c5a89bde12
Julia Gambuzzi <jgambuzzi@jgambuzzi-ltm.internal.salesforce.com>
2019-12-13 13:50:21 -0300

    fix compilation

--- src/main/java/ar/edu/unahur/obj2/ejercicio2/Matencio.java
+++ src/main/java/ar/edu/unahur/obj2/ejercicio2/Matencio.java
@@ -21,38 +21,38 @@ public class Matencio{
     }
 
     private void llenarTermo(StringBuffer instrucciones ){
-        instrucciones.append(("Llene el termo con el agua caliente");
+        instrucciones.append("Llene el termo con el agua caliente");
     }
 
     private void agregarYerbaAlMate(StringBuffer instrucciones ){
-        instrucciones.append(("Incline el mate a 45 grados y llene 3/4 partes de yerba");
+        instrucciones.append("Incline el mate a 45 grados y llene 3/4 partes de yerba");
     }
 
     private void agregarSaboresSiHay(TipoMate tipoMate, StringBuffer instrucciones ){
 
         if(TipoMate.CITRIC.equals(tipoMate)){
-            instrucciones.append(("Agregue las cáscaras de naranja a la yerba, mezcle");
+            instrucciones.append("Agregue las cáscaras de naranja a la yerba, mezcle");
         }else if(TipoMate.MENTA.equals(tipoMate)){
-            instrucciones.append(("Agregue la menta a la yerba, mezcle");
+            instrucciones.append("Agregue la menta a la yerba, mezcle");
         }else if(TipoMate.PEPERINA.equals(tipoMate)){
-            instrucciones.append(("Agregue la peperina a la yerba, mezcle");
+            instrucciones.append("Agregue la peperina a la yerba, mezcle");
         }else if(TipoMate.DULCE.equals(tipoMate)){
-            instrucciones.append(("Agregue azucar a la yerba, repita esta operación antes de cada cebada");
+            instrucciones.append("Agregue azucar a la yerba, repita esta operación antes de cada cebada");
         }else {
             throw new RuntimeException("Tipo de mate inválido");
         }
     }
 
     private void mojarElMate(StringBuffer instrucciones ){
-        instrucciones.append(("Moje el mate lentamente del lado que el nivel de yerba es más bajo 3 veces dejando que la yerba se humedezca");
+        instrucciones.append("Moje el mate lentamente del lado que el nivel de yerba es más bajo 3 veces dejando que la yerba se humedezca");
     }
 
     private void ponerLaBombilla(StringBuffer instrucciones ){
-        instrucciones.append(("Inserte la bombilla en la yerba húmeda");
+        instrucciones.append("Inserte la bombilla en la yerba húmeda");
     }
 
     private void cebar(StringBuffer instrucciones ){
-        instrucciones.append(("Ya puede comenzar la ronda");
+        instrucciones.append("Ya puede comenzar la ronda");
     }
 
 
